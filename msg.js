002cacc09be8862a239efcccfc1e63364f4d9690
Sunitha <136448809+SunithaBisinigi@users.noreply.github.com>
2023-06-13 16:07:11 +0530

    Add files via upload

---
+++ msg.js
@@ -0,0 +1,6 @@
+let change = document.getElementsByTagName("p");
+let b = document.getElementById("b");
+b.onclick = function(){
+    change[0].innerHTML="I am sunitha from navgurukul campus Pune";
+    change[0].style.color="white";
+};
